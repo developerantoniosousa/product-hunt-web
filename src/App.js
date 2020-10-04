d8b55c5fc1b817d44189278bd3761cede9521adc
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-04 14:58:18 -0300

    feat: routing seted up

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from "react";
 import "./styles.css";
 
 import Header from "./components/Header";
-import Main from "./pages/Main";
+import Routes from "./routes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
