81ec066d51b6e99dbd7401e5ecaf50b67dcfc642
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-04 15:12:16 -0300

    feat: add Product page

---
+++ src/pages/Product/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+import api from "../../services/api";
+
+class Product extends Component {
+  state = {
+    product: {},
+  };
+
+  componentDidMount() {
+    this.loadProduct();
+  }
+
+  loadProduct = async () => {
+    const { id } = this.props.match.params;
+    const response = await api.get(`/products/${id}`);
+    this.setState({ product: response.data });
+  };
+
+  render() {
+    const { product } = this.state;
+    return (
+      <div className="product-info">
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <p>
+          URL:{" "}
+          <a href={product.url} target="_blank">
+            {product.url}
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Product;
